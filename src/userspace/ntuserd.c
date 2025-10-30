010cd410f332838503a6dfed79c91b0fb12d748d
lanxia404 <lanxia657@gmail.com>
2025-10-31 03:36:24 +0800

    Enhance PE loader parsing

--- src/userspace/ntuserd.c
+++ src/userspace/ntuserd.c
@@ -1,3 +1,5 @@
+#include <errno.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -35,54 +37,155 @@ struct nt_mem_info {
     unsigned long type;
 };
 
+#define IMAGE_DOS_SIGNATURE 0x5A4D
+#define IMAGE_NT_SIGNATURE  0x00004550
+#define IMAGE_NUMBEROF_DIRECTORY_ENTRIES 16
+#define IMAGE_NT_OPTIONAL_HDR32_MAGIC 0x10B
+#define IMAGE_NT_OPTIONAL_HDR64_MAGIC 0x20B
+
 // PE 檔案結構定義
-typedef struct {
-    unsigned short e_magic;      // Magic number
-    unsigned short e_cblp;       // Bytes on last page of file
-    unsigned short e_cp;         // Pages in file
-    unsigned short e_crlc;       // Relocations
-    unsigned short e_cparhdr;    // Size of header in paragraphs
-    unsigned short e_minalloc;   // Minimum extra paragraphs needed
-    unsigned short e_maxalloc;   // Maximum extra paragraphs needed
-    unsigned short e_ss;         // Initial (relative) SS value
-    unsigned short e_sp;         // Initial SP value
-    unsigned short e_csum;       // Checksum
-    unsigned short e_ip;         // Initial IP value
-    unsigned short e_cs;         // Initial (relative) CS value
-    unsigned short e_lfarlc;     // File address of relocation table
-    unsigned short e_ovno;       // Overlay number
-    unsigned short e_res[4];     // Reserved words
-    unsigned short e_oemid;      // OEM identifier (for e_oeminfo)
-    unsigned short e_oeminfo;    // OEM information; e_oemid specific
-    unsigned short e_res2[10];   // Reserved words
-    long e_lfanew;               // File address of new exe header
+typedef struct __attribute__((packed)) {
+    uint16_t e_magic;      // Magic number
+    uint16_t e_cblp;       // Bytes on last page of file
+    uint16_t e_cp;         // Pages in file
+    uint16_t e_crlc;       // Relocations
+    uint16_t e_cparhdr;    // Size of header in paragraphs
+    uint16_t e_minalloc;   // Minimum extra paragraphs needed
+    uint16_t e_maxalloc;   // Maximum extra paragraphs needed
+    uint16_t e_ss;         // Initial (relative) SS value
+    uint16_t e_sp;         // Initial SP value
+    uint16_t e_csum;       // Checksum
+    uint16_t e_ip;         // Initial IP value
+    uint16_t e_cs;         // Initial (relative) CS value
+    uint16_t e_lfarlc;     // File address of relocation table
+    uint16_t e_ovno;       // Overlay number
+    uint16_t e_res[4];     // Reserved words
+    uint16_t e_oemid;      // OEM identifier (for e_oeminfo)
+    uint16_t e_oeminfo;    // OEM information; e_oemid specific
+    uint16_t e_res2[10];   // Reserved words
+    int32_t e_lfanew;      // File address of new exe header
 } IMAGE_DOS_HEADER, *PIMAGE_DOS_HEADER;
 
-// NT 頭部結構 (簡化版)
-typedef struct {
-    unsigned long Signature;
-    unsigned short Machine;
-    unsigned short NumberOfSections;
-    unsigned long TimeDateStamp;
-    unsigned long PointerToSymbolTable;
-    unsigned long NumberOfSymbols;
-    unsigned short SizeOfOptionalHeader;
-    unsigned short Characteristics;
-} IMAGE_NT_HEADERS, *PIMAGE_NT_HEADERS;
+typedef struct __attribute__((packed)) {
+    uint32_t VirtualAddress;
+    uint32_t Size;
+} IMAGE_DATA_DIRECTORY, *PIMAGE_DATA_DIRECTORY;
 
-// PE 區段頭部結構
-typedef struct {
+typedef struct __attribute__((packed)) {
+    uint16_t Machine;
+    uint16_t NumberOfSections;
+    uint32_t TimeDateStamp;
+    uint32_t PointerToSymbolTable;
+    uint32_t NumberOfSymbols;
+    uint16_t SizeOfOptionalHeader;
+    uint16_t Characteristics;
+} IMAGE_FILE_HEADER, *PIMAGE_FILE_HEADER;
+
+typedef struct __attribute__((packed)) {
+    uint16_t Magic;
+    uint8_t MajorLinkerVersion;
+    uint8_t MinorLinkerVersion;
+    uint32_t SizeOfCode;
+    uint32_t SizeOfInitializedData;
+    uint32_t SizeOfUninitializedData;
+    uint32_t AddressOfEntryPoint;
+    uint32_t BaseOfCode;
+    uint32_t BaseOfData;
+    uint32_t ImageBase;
+    uint32_t SectionAlignment;
+    uint32_t FileAlignment;
+    uint16_t MajorOperatingSystemVersion;
+    uint16_t MinorOperatingSystemVersion;
+    uint16_t MajorImageVersion;
+    uint16_t MinorImageVersion;
+    uint16_t MajorSubsystemVersion;
+    uint16_t MinorSubsystemVersion;
+    uint32_t Win32VersionValue;
+    uint32_t SizeOfImage;
+    uint32_t SizeOfHeaders;
+    uint32_t CheckSum;
+    uint16_t Subsystem;
+    uint16_t DllCharacteristics;
+    uint32_t SizeOfStackReserve;
+    uint32_t SizeOfStackCommit;
+    uint32_t SizeOfHeapReserve;
+    uint32_t SizeOfHeapCommit;
+    uint32_t LoaderFlags;
+    uint32_t NumberOfRvaAndSizes;
+    IMAGE_DATA_DIRECTORY DataDirectory[IMAGE_NUMBEROF_DIRECTORY_ENTRIES];
+} IMAGE_OPTIONAL_HEADER32, *PIMAGE_OPTIONAL_HEADER32;
+
+typedef struct __attribute__((packed)) {
+    uint16_t Magic;
+    uint8_t MajorLinkerVersion;
+    uint8_t MinorLinkerVersion;
+    uint32_t SizeOfCode;
+    uint32_t SizeOfInitializedData;
+    uint32_t SizeOfUninitializedData;
+    uint32_t AddressOfEntryPoint;
+    uint32_t BaseOfCode;
+    uint64_t ImageBase;
+    uint32_t SectionAlignment;
+    uint32_t FileAlignment;
+    uint16_t MajorOperatingSystemVersion;
+    uint16_t MinorOperatingSystemVersion;
+    uint16_t MajorImageVersion;
+    uint16_t MinorImageVersion;
+    uint16_t MajorSubsystemVersion;
+    uint16_t MinorSubsystemVersion;
+    uint32_t Win32VersionValue;
+    uint32_t SizeOfImage;
+    uint32_t SizeOfHeaders;
+    uint32_t CheckSum;
+    uint16_t Subsystem;
+    uint16_t DllCharacteristics;
+    uint64_t SizeOfStackReserve;
+    uint64_t SizeOfStackCommit;
+    uint64_t SizeOfHeapReserve;
+    uint64_t SizeOfHeapCommit;
+    uint32_t LoaderFlags;
+    uint32_t NumberOfRvaAndSizes;
+    IMAGE_DATA_DIRECTORY DataDirectory[IMAGE_NUMBEROF_DIRECTORY_ENTRIES];
+} IMAGE_OPTIONAL_HEADER64, *PIMAGE_OPTIONAL_HEADER64;
+
+typedef struct __attribute__((packed)) {
     char Name[8];
-    unsigned long VirtualSize;
-    unsigned long VirtualAddress;
-    unsigned long SizeOfRawData;
-    unsigned long PointerToRawData;
-    unsigned long PointerToRelocations;
-    unsigned long PointerToLinenumbers;
-    unsigned short NumberOfRelocations;
-    unsigned short NumberOfLinenumbers;
-    unsigned long Characteristics;
+    uint32_t VirtualSize;
+    uint32_t VirtualAddress;
+    uint32_t SizeOfRawData;
+    uint32_t PointerToRawData;
+    uint32_t PointerToRelocations;
+    uint32_t PointerToLinenumbers;
+    uint16_t NumberOfRelocations;
+    uint16_t NumberOfLinenumbers;
+    uint32_t Characteristics;
 } IMAGE_SECTION_HEADER, *PIMAGE_SECTION_HEADER;
+
+typedef struct {
+    uint8_t *image_base;
+    size_t image_size;
+    uint64_t preferred_base;
+    uint64_t entry_point_rva;
+    int is_pe64;
+} pe_image;
+
+static void free_pe_image(pe_image *image) {
+    if (!image) {
+        return;
+    }
+    free(image->image_base);
+    image->image_base = NULL;
+    image->image_size = 0;
+}
+
+static int allocate_pe_image(pe_image *image, size_t size) {
+    image->image_base = calloc(1, size);
+    if (!image->image_base) {
+        return -1;
+    }
+    image->image_size = size;
+    return 0;
+}
 
 int ntcore_fd = -1;
 
@@ -101,6 +204,51 @@ void cleanup_ntcore_device() {
     }
 }
 
+static void dump_section_info(const IMAGE_SECTION_HEADER *section) {
+    char name[9];
+    memset(name, 0, sizeof(name));
+    memcpy(name, section->Name, sizeof(section->Name));
+
+    printf("  Section %-8s | RVA 0x%08X | RawSize 0x%08X | Characteristics 0x%08X\n",
+           name,
+           section->VirtualAddress,
+           section->SizeOfRawData,
+           section->Characteristics);
+}
+
+static int copy_section_into_image(FILE *file, const IMAGE_SECTION_HEADER *section, pe_image *image) {
+    size_t raw_size = section->SizeOfRawData;
+    size_t virtual_size = section->VirtualSize ? section->VirtualSize : raw_size;
+    size_t section_end = (size_t)section->VirtualAddress + virtual_size;
+
+    if (section_end > image->image_size || section_end < section->VirtualAddress) {
+        fprintf(stderr, "Section does not fit in allocated image: RVA=0x%X Size=0x%zX ImageSize=0x%zX\n",
+                section->VirtualAddress,
+                virtual_size,
+                image->image_size);
+        return -1;
+    }
+
+    if (raw_size == 0) {
+        return 0;
+    }
+
+    if (fseek(file, section->PointerToRawData, SEEK_SET) != 0) {
+        perror("Failed to seek to section data");
+        return -1;
+    }
+
+    if (fread(image->image_base + section->VirtualAddress,
+              raw_size,
+              1,
+              file) != 1) {
+        perror("Failed to read section data");
+        return -1;
+    }
+
+    return 0;
+}
+
 // 模擬 PE Loader 功能
 int load_pe_file(const char *filename) {
     FILE *file = fopen(filename, "rb");
@@ -116,17 +264,158 @@ int load_pe_file(const char *filename) {
         return -1;
     }
 
-    if (dos_header.e_magic != 0x5A4D) { // "MZ"
+    if (dos_header.e_magic != IMAGE_DOS_SIGNATURE) {
         fprintf(stderr, "Not a valid PE file (DOS header)\n");
         fclose(file);
         return -1;
     }
 
-    printf("PE file loaded: %s\n", filename);
-    printf("DOS Header Magic: 0x%04X\n", dos_header.e_magic);
-    printf("NT Header offset: 0x%08lX\n", dos_header.e_lfanew);
+    if (fseek(file, dos_header.e_lfanew, SEEK_SET) != 0) {
+        perror("Failed to seek to NT headers");
+        fclose(file);
+        return -1;
+    }
 
-    // TODO: 完成 PE 檔案解析邏輯
+    uint32_t signature = 0;
+    if (fread(&signature, sizeof(signature), 1, file) != 1) {
+        perror("Failed to read NT signature");
+        fclose(file);
+        return -1;
+    }
+
+    if (signature != IMAGE_NT_SIGNATURE) {
+        fprintf(stderr, "Not a valid PE file (NT signature mismatch)\n");
+        fclose(file);
+        return -1;
+    }
+
+    IMAGE_FILE_HEADER file_header;
+    if (fread(&file_header, sizeof(file_header), 1, file) != 1) {
+        perror("Failed to read file header");
+        fclose(file);
+        return -1;
+    }
+
+    printf("PE file loaded: %s\n", filename);
+    printf("Machine: 0x%04X | Sections: %u | OptionalHeaderSize: %u bytes\n",
+           file_header.Machine,
+           file_header.NumberOfSections,
+           file_header.SizeOfOptionalHeader);
+
+    if (file_header.NumberOfSections == 0) {
+        fprintf(stderr, "PE file does not contain any section\n");
+        fclose(file);
+        return -1;
+    }
+
+    uint8_t *optional_header_raw = malloc(file_header.SizeOfOptionalHeader);
+    if (!optional_header_raw) {
+        perror("Failed to allocate optional header buffer");
+        fclose(file);
+        return -1;
+    }
+
+    if (fread(optional_header_raw, file_header.SizeOfOptionalHeader, 1, file) != 1) {
+        perror("Failed to read optional header");
+        free(optional_header_raw);
+        fclose(file);
+        return -1;
+    }
+
+    pe_image image = {0};
+
+    uint16_t optional_magic = *(uint16_t *)optional_header_raw;
+    if (optional_magic == IMAGE_NT_OPTIONAL_HDR32_MAGIC) {
+        if (file_header.SizeOfOptionalHeader < sizeof(IMAGE_OPTIONAL_HEADER32)) {
+            fprintf(stderr, "Optional header too small for PE32 format\n");
+            free(optional_header_raw);
+            fclose(file);
+            return -1;
+        }
+
+        IMAGE_OPTIONAL_HEADER32 *opt32 = (IMAGE_OPTIONAL_HEADER32 *)optional_header_raw;
+        printf("PE32 Optional Header: EntryPoint RVA 0x%08X | ImageBase 0x%08X | SizeOfImage 0x%08X\n",
+               opt32->AddressOfEntryPoint,
+               opt32->ImageBase,
+               opt32->SizeOfImage);
+        image.preferred_base = opt32->ImageBase;
+        image.entry_point_rva = opt32->AddressOfEntryPoint;
+        image.is_pe64 = 0;
+
+        if (allocate_pe_image(&image, opt32->SizeOfImage) != 0) {
+            fprintf(stderr, "Failed to allocate memory for PE image of size 0x%X\n", opt32->SizeOfImage);
+            free(optional_header_raw);
+            fclose(file);
+            return -1;
+        }
+    } else if (optional_magic == IMAGE_NT_OPTIONAL_HDR64_MAGIC) {
+        if (file_header.SizeOfOptionalHeader < sizeof(IMAGE_OPTIONAL_HEADER64)) {
+            fprintf(stderr, "Optional header too small for PE32+ format\n");
+            free(optional_header_raw);
+            fclose(file);
+            return -1;
+        }
+
+        IMAGE_OPTIONAL_HEADER64 *opt64 = (IMAGE_OPTIONAL_HEADER64 *)optional_header_raw;
+        printf("PE32+ Optional Header: EntryPoint RVA 0x%08X | ImageBase 0x%llX | SizeOfImage 0x%08X\n",
+               opt64->AddressOfEntryPoint,
+               (unsigned long long)opt64->ImageBase,
+               opt64->SizeOfImage);
+        image.preferred_base = opt64->ImageBase;
+        image.entry_point_rva = opt64->AddressOfEntryPoint;
+        image.is_pe64 = 1;
+
+        if (allocate_pe_image(&image, opt64->SizeOfImage) != 0) {
+            fprintf(stderr, "Failed to allocate memory for PE image of size 0x%X\n", opt64->SizeOfImage);
+            free(optional_header_raw);
+            fclose(file);
+            return -1;
+        }
+    } else {
+        fprintf(stderr, "Unsupported optional header magic: 0x%04X\n", optional_magic);
+        free(optional_header_raw);
+        fclose(file);
+        return -1;
+    }
+
+    free(optional_header_raw);
+
+    IMAGE_SECTION_HEADER *sections = calloc(file_header.NumberOfSections, sizeof(IMAGE_SECTION_HEADER));
+    if (!sections) {
+        perror("Failed to allocate section headers");
+        free_pe_image(&image);
+        fclose(file);
+        return -1;
+    }
+
+    if (fread(sections, sizeof(IMAGE_SECTION_HEADER), file_header.NumberOfSections, file) != file_header.NumberOfSections) {
+        perror("Failed to read section headers");
+        free(sections);
+        free_pe_image(&image);
+        fclose(file);
+        return -1;
+    }
+
+    printf("Sections:\n");
+    for (uint16_t i = 0; i < file_header.NumberOfSections; ++i) {
+        dump_section_info(&sections[i]);
+        if (copy_section_into_image(file, &sections[i], &image) != 0) {
+            fprintf(stderr, "Failed to load section %u\n", i);
+            free(sections);
+            free_pe_image(&image);
+            fclose(file);
+            return -1;
+        }
+    }
+
+    uint64_t entry_point = image.preferred_base + image.entry_point_rva;
+    printf("PE image mapped at 0x%llX (preferred base 0x%llX). Entry point: 0x%llX\n",
+           (unsigned long long)(uintptr_t)image.image_base,
+           (unsigned long long)image.preferred_base,
+           (unsigned long long)entry_point);
+
+    free(sections);
+    free_pe_image(&image);
     fclose(file);
     return 0;
 }
